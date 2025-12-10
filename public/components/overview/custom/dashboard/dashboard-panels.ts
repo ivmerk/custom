f8aaeb5af5bc497cb817c94dd68436467afa9387
ivmerk <ivan.e.merkulov@gmail.com>
2025-12-10 10:04:21 +0200

    dashboard started

---
+++ public/components/overview/custom/dashboard/dashboard-panels.ts
@@ -0,0 +1,203 @@
+import { DashboardPanelState } from '../../../../../../../../src/plugins/dashboard/public/application';
+import { EmbeddableInput } from '../../../../../../../../src/plugins/embeddable/public';
+
+const getVisStateAlertsEvolution = (indexPatternId: string) => {
+  return {
+    id: 'Wazuh-App-Overview-MITRE-Alerts-Evolution',
+    title: 'Alerts evolution over time',
+    type: 'line',
+    params: {
+      type: 'line',
+      grid: { categoryLines: false },
+      categoryAxes: [
+        {
+          id: 'CategoryAxis-1',
+          type: 'category',
+          position: 'bottom',
+          show: true,
+          style: {},
+          scale: { type: 'linear' },
+          labels: { show: true, filter: true, truncate: 100 },
+          title: {},
+        },
+      ],
+      valueAxes: [
+        {
+          id: 'ValueAxis-1',
+          name: 'LeftAxis-1',
+          type: 'value',
+          position: 'left',
+          show: true,
+          style: {},
+          scale: { type: 'linear', mode: 'normal' },
+          labels: { show: true, rotate: 0, filter: false, truncate: 100 },
+          title: { text: 'Count' },
+        },
+      ],
+      seriesParams: [
+        {
+          show: 'true',
+          type: 'line',
+          mode: 'normal',
+          data: { label: 'Count', id: '1' },
+          valueAxis: 'ValueAxis-1',
+          drawLinesBetweenPoints: true,
+          showCircles: true,
+          lineWidth: 2,
+        },
+      ],
+      addTooltip: true,
+      addLegend: true,
+      legendPosition: 'right',
+      times: [],
+      addTimeMarker: false,
+      labels: {},
+      thresholdLine: {
+        show: false,
+        value: 10,
+        width: 1,
+        style: 'full',
+        color: '#34130C',
+      },
+      dimensions: {
+        x: {
+          accessor: 0,
+          format: { id: 'date', params: { pattern: 'YYYY-MM-DD HH:mm' } },
+          params: {
+            date: true,
+            interval: 'PT3H',
+            format: 'YYYY-MM-DD HH:mm',
+            bounds: {
+              min: '2019-11-07T15:45:45.770Z',
+              max: '2019-11-14T15:45:45.770Z',
+            },
+          },
+          aggType: 'date_histogram',
+        },
+        y: [
+          {
+            accessor: 2,
+            format: { id: 'number' },
+            params: {},
+            aggType: 'count',
+          },
+        ],
+        series: [
+          {
+            accessor: 1,
+            format: {
+              id: 'terms',
+              params: {
+                id: 'string',
+                otherBucketLabel: 'Other',
+                missingBucketLabel: 'Missing',
+              },
+            },
+            params: {},
+            aggType: 'terms',
+          },
+        ],
+      },
+    },
+    data: {
+      searchSource: {
+        query: {
+          language: 'kuery',
+          query: '',
+        },
+        filter: [],
+        index: indexPatternId,
+      },
+      references: [
+        {
+          name: 'kibanaSavedObjectMeta.searchSourceJSON.index',
+          type: 'index-pattern',
+          id: indexPatternId,
+        },
+      ],
+      aggs: [
+        {
+          id: '1',
+          enabled: true,
+          type: 'count',
+          schema: 'metric',
+          params: {},
+        },
+        {
+          id: '3',
+          enabled: true,
+          type: 'terms',
+          schema: 'group',
+          params: {
+            field: 'rule.mitre.technique',
+            customLabel: 'Attack ID',
+            orderBy: '1',
+            order: 'desc',
+            size: 5,
+            otherBucket: false,
+            otherBucketLabel: 'Other',
+            missingBucket: false,
+            missingBucketLabel: 'Missing',
+          },
+        },
+        {
+          id: '2',
+          enabled: true,
+          type: 'date_histogram',
+          schema: 'segment',
+          params: {
+            field: 'timestamp',
+            timeRange: { from: 'now-7d', to: 'now' },
+            useNormalizedEsInterval: true,
+            interval: 'auto',
+            drop_partials: false,
+            min_doc_count: 1,
+            extended_bounds: {},
+          },
+        },
+      ],
+    },
+  };
+};
+export const getDashboardPanels = (
+  indexPatternId: string,
+  pinnedAgent?: boolean,
+): {
+  [panelId: string]: DashboardPanelState<
+    EmbeddableInput & { [k: string]: unknown }
+  >;
+} => {
+  const pinnedAgentPanels = {
+    '1': {
+      gridData: {
+        w: 36,
+        h: 12,
+        x: 0,
+        y: 0,
+        i: '1',
+      },
+      type: 'visualization',
+      explicitInput: {
+        id: '1',
+        savedVis: getVisStateAlertsEvolution(indexPatternId),
+      },
+    },
+  };
+  const panels = {
+    '6': {
+      gridData: {
+        w: 36,
+        h: 12,
+        x: 0,
+        y: 0,
+        i: '6',
+      },
+      type: 'visualization',
+      explicitInput: {
+        id: '6',
+        savedVis: getVisStateAlertsEvolution(indexPatternId),
+      },
+    },
+  };
+  return pinnedAgent ? pinnedAgentPanels : panels;
+};
--- public/components/overview/custom/dashboard/dashboard.tsx
+++ public/components/overview/custom/dashboard/dashboard.tsx
@@ -1,27 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getPlugins } from '../../../../kibana-services';
 import { I18nProvider } from '@osd/i18n/react';
+import { ViewMode } from '../../../../../../../src/plugins/embeddable/public';
+import { SearchResponse } from '../../../../../../../src/core/server';
 import { withErrorBoundary } from '../../../common/hocs/error-boundary/with-error-boundary';
 import { DiscoverNoResults } from '../../../common/no-results/no-results';
+import { IndexPattern } from '../../../../../../../src/plugins/data/common';
+import './custom_dashboard_filters.scss';
 import {
   AlertsDataSourceRepository,
   CustomDataSource,
+  PatternDataSource,
+  tParsedIndexPattern,
   useDataSource
 } from '../../../common/data-source';
+import { WzSearchBar } from '../../../common/search-bar';
+import useSearchBar from '../../../common/search-bar/use-search-bar';
+import { SampleDataWarning } from '../../../visualize/components';
+import { getDashboardPanels } from './dashboard-panels';
 
 const plugins = getPlugins();
 
 const DashboardByRenderer = plugins.dashboard.DashboardContainerByValueRenderer;
-  const DashboardC: React.FC = () => {
-    const AlertsRepository = new AlertsDataSourceRepository();
-  const {dataSource} = useDataSource({DataSource: CustomDataSource, repository: AlertsRepository});
+
+const DashboardC: React.FC = () => {
+  const AlertsRepository = new AlertsDataSourceRepository();
+  const {
+    filters,
+    setFilters,
+    fetchFilters,
+    fixedFilters,
+    dataSource
+  } = useDataSource<tParsedIndexPattern, PatternDataSource>({
+    DataSource: CustomDataSource, repository: AlertsRepository
+  });
+  console.log("dataSource = ", dataSource, "filters = ", filters);
+  const [results, setResults] = useState<SearchResponse>({} as SearchResponse);
+
+  const { searchBarProps, fingerprint, autoRefreshFingerprint } = useSearchBar({
+    indexPattern: dataSource?.indexPattern as IndexPattern,
+    filters,
+    setFilters,
+  });
+  const { query, dateRangeFrom, dateRangeTo } = searchBarProps;
 
   return (
     <I18nProvider>
-      <div>
-        <h1>Dashboard Custom</h1>
-        <DiscoverNoResults />
-      </div>
+      <>
+        <WzSearchBar
+          appName='custom-searchbar'
+          {...searchBarProps}
+          fixedFilters={fixedFilters}
+          showQueryInput={true}
+          showQueryBar={true}
+          showSaveQuery={true}
+        />
+        { dataSource ? <DiscoverNoResults /> : null }
+        <div
+          className={`custom-dashboard-responsive ${
+            dataSource && results?.hits?.total > 0 ? '' : 'wz-no-display'
+          }`}
+        >
+          <SampleDataWarning />
+          <div className='custom-dashboard-filters-wrapper'>
+            <DashboardByRenderer
+              input={{
+                viewMode: ViewMode.VIEW,
+                panels: getDashboardPanels(
+                  AlertsRepository.getStoreIndexPatternId(),
+                  Boolean(dataSource?.getPinnedAgentFilter().length),
+                ),
+                isFullScreenMode: false,
+                filters: fetchFilters ?? [],
+                useMargins: true,
+                id: 'custom-dashboard-tab-filters',
+                timeRange: { from: dateRangeFrom, to: dateRangeTo },
+                title: 'Custom dashboard filters',
+                description: 'Dashboard of the customs filters',
+                query: query,
+                refreshConfig: {
+                  pause: false,
+                  value: 15,
+                },
+                hidePanelTitles: false,
+                lastReloadRequestTime: fingerprint,
+              }}
+            />
+          </div>
+        </div>
+      </>
     </I18nProvider>
   );
 };
